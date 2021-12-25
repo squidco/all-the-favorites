7045b20a5a6e18f53df7785bb1d90fbf629ac5fe
Anthony Pillow <anthonypillow18@gmail.com>
2021-12-25 00:56:10 -0800

    Working on card design more. Have a gradient and well positioned text. Need sample bg images.

--- src/components/FavoriteCard.js
+++ src/components/FavoriteCard.js
@@ -1,11 +1,14 @@
 import "./FavoriteCard.css";
-import "../assets/style.css"
+import "../assets/style.css";
 
 function FavoriteCard({ info }) {
   // Needs title and picture link or color
   return (
-    <div className={`${info.pic} card-body`}>
+    <div className={`${info.pic} card`}>
       <h1 className="card-title">{info.title}</h1>
+      <div className="card-body">
+        <p className="card-description">{info.description}</p>
+      </div>
     </div>
   );
 }
